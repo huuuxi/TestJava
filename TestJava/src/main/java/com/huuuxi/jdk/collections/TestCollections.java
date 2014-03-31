57283e0b449316f23320502d3874df11b8fa0890
huuuxi <huuuxi@163.com>
2014-04-01 00:44:10 +0800

    方法

--- TestJava/src/main/java/com/huuuxi/jdk/collections/TestCollections.java
+++ TestJava/src/main/java/com/huuuxi/jdk/collections/TestCollections.java
@@ -1,5 +1,6 @@
 package com.huuuxi.jdk.collections;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -8,8 +9,22 @@ import java.util.Collections;
  */
 public class TestCollections {
 
+	public static void main(String[] args) {
+		testCollection();
+	}
 	public static void testCollection(){
+		System.out.println(System.getenv("JAVA_HOME"));
+		System.out.println(System.getProperties());
+		// 看到 Collections 方法，自然用到了 Arrays里面方法；
 		Collections collections;
+		Arrays arrays;
+		Arrays.copyOf(new int[]{1}, 2);
+		//使用的是System.arraycopy
+		int[] a = {}; int b[] = {};
+		System.arraycopy(a, 0, b, 0, 1);
+		// 
+		System.out.print(true);
+		
 		
 	}
 }
