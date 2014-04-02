bbee0936ed477e6e13615b4adeef387a282c7581
huuuxi <huuuxi@gmail.com>
2014-04-02 10:04:58 +0800

    1

--- TestJava/src/main/java/com/huuuxi/jdk/collections/TestCollections.java
+++ TestJava/src/main/java/com/huuuxi/jdk/collections/TestCollections.java
@@ -7,9 +7,14 @@ import java.util.Collections;
  *
  */
 public class TestCollections {
-
+/***
+ *Collections ： serarch，list必须是有序的sorted，如果 实现RandomAccess 会快 在 log(n)；否则在 n和log(n)之间
+ * 里面很多方法，判断是否实现了 RandomAccess，如果实现则 get(i),否则 Iterator
+ */
 	public static void testCollection(){
 		Collections collections;
 		
 	}
+	
+	
 }
