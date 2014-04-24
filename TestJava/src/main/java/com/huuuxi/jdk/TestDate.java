f63c47ff4817e1ec22079093a751a8e1e10e0d9b
huuuxi <huuuxi@gmail.com>
2014-04-24 21:27:51 +0800

    Signed-off-by: huuuxi <huuuxi@gmail.com>

--- TestJava/src/main/java/com/huuuxi/jdk/TestDate.java
+++ TestJava/src/main/java/com/huuuxi/jdk/TestDate.java
@@ -1,12 +1,31 @@
 package com.huuuxi.jdk;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class TestDate {
 
-	private static Calendar cla = Calendar.getInstance();
 
-	
+	public static void main(String[] args) {
+		Calendar cla = Calendar.getInstance();
+		cla.set(Calendar.HOUR_OF_DAY,0);
+		cla.set(Calendar.MINUTE,0);
+		cla.set(Calendar.SECOND,0);
+		cla.set(Calendar.MILLISECOND,0);
+		Date date = cla.getTime();
+		System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:SS").format(date));
+		System.out.println(date);
+		cla.add(Calendar.DATE, -1);
+		date = cla.getTime();
+		System.out.println(date);
+		cla.add(Calendar.DATE, -1);
+		date = cla.getTime();	
+		System.out.println(date);	
+		cla.add(Calendar.DATE, -1);
+		date = cla.getTime();	
+		System.out.println(date);	
+	}
 	
 	
 }
