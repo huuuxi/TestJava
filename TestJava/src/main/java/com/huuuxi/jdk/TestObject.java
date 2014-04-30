1dba729a5ad6e653eae784ce5838bd35748dfccf
huuuxi <huuuxi@gmail.com>
2014-04-30 21:11:23 +0800

    Signed-off-by: huuuxi <huuuxi@gmail.com>

--- TestJava/src/main/java/com/huuuxi/jdk/TestObject.java
+++ TestJava/src/main/java/com/huuuxi/jdk/TestObject.java
@@ -3,7 +3,7 @@ package com.huuuxi.jdk;
 public class TestObject {
 
 	public static void main(String[] args) {
-		
+		testZ();
 	}
 	
 	/**
@@ -14,4 +14,30 @@ public class TestObject {
 	public static void testObject(){
 		
 	}
+	
+	public static void testZ(){
+//			String t = "123";
+//			String s = "321";
+		Strings  t = new Strings();
+		t.ts = "a";
+		Strings s = new Strings();
+		s.ts = "b";
+		System.out.printf("t: %s, s: %s;\n",t.ts,s.ts);
+			swap(t, s);
+			System.out.printf("t: %s, s: %s;\n",t.ts,s.ts);
+		
+	}
+	
+	public static void swap(Strings t ,Strings s){
+//		String temp = s.ts;
+//		s.ts = t.ts;
+//		t.ts = temp;
+		Strings temp = s;
+		s = t;
+		t = temp;;
+	}
+}
+
+class Strings {
+	String ts ;
 }
